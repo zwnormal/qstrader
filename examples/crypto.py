947b7f69e321b5776bb3d323403d4cf8f80bf24f
Wei Zheng <zwnormal@gmail.com>
2024-07-24 20:36:38 +1000

    Change to do short as well

--- examples/crypto.py
+++ examples/crypto.py
@@ -29,13 +29,13 @@ class GRUAlphaModel(AlphaModel):
 
         if self.signals.warmup >= lookback:
             assets = self.signals["gru"].assets
-            decision = self.signals["gru"]("EQ:SPY", lookback)
+            decision = self.signals["gru"]("EQ:ETH-USD", lookback)
 
             if decision == -1:
-                weights["EQ:SPY"] = 0.0
-                weights["EQ:USDC-USD"] = 1 - weights[assets[0]]
+                weights["EQ:ETH-USD"] = -0.5
+                weights["EQ:USDC-USD"] = 1
             elif decision == 1:
-                weights["EQ:SPY"] = 0.8
+                weights["EQ:ETH-USD"] = 0.8
                 weights["EQ:USDC-USD"] = 1 - weights[assets[0]]
             else:
                 return {}
@@ -43,13 +43,13 @@ class GRUAlphaModel(AlphaModel):
 
 
 if __name__ == "__main__":
-    start_dt = pd.Timestamp("2018-10-08 14:30:00", tz=pytz.UTC)
-    burn_in_dt = pd.Timestamp("2019-07-08 14:30:00", tz=pytz.UTC)
+    start_dt = pd.Timestamp("2017-11-09 14:30:00", tz=pytz.UTC)
+    burn_in_dt = pd.Timestamp("2018-06-09 14:30:00", tz=pytz.UTC)
     end_dt = pd.Timestamp("2024-07-10 23:59:00", tz=pytz.UTC)
 
     lookback = 365
 
-    strategy_symbols = ["SPY", "USDC-USD"]
+    strategy_symbols = ["ETH-USD", "USDC-USD"]
     strategy_assets = ["EQ:%s" % symbol for symbol in strategy_symbols]
     strategy_universe = StaticUniverse(strategy_assets)
 
@@ -86,7 +86,8 @@ if __name__ == "__main__":
         strategy_alpha_model,
         signals=signals,
         rebalance="daily",
-        long_only=True,
+        long_only=False,
+        gross_leverage=1.0,
         cash_buffer_percentage=0.01,
         burn_in_dt=burn_in_dt,
         data_handler=strategy_data_handler,
